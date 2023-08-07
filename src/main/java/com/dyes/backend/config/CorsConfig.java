f9b584e240e6379bbbc5f0a17f12f03da02e3dd0
jeongdawun <jeongdawun33@gmail.com>
2023-08-07 16:10:46 +0900

    init: [정다운] Spring Boot 초기 설정 [IDY-1]

---
+++ src/main/java/com/dyes/backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.dyes.backend.config;
+
+import com.dyes.backend.utility.PropertyUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final PropertyUtil propertyUtil;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        final String ALLOWED_ORIGINS = propertyUtil.getProperty("allowed_origins");
+
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
---
+++ src/main/java/com/dyes/backend/utility/PropertyUtil.java
@@ -0,0 +1,18 @@
+package com.dyes.backend.utility;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@RequiredArgsConstructor
+@PropertySource(value = {"cors.properties"})
+public class PropertyUtil {
+
+    final private Environment environment;
+
+    public String getProperty(String key) {
+        return environment.getProperty(key);
+    }
+}
