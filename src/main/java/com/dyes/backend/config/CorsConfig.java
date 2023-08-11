5a86341ca927bd3a6d99985c3c20d7cec8c47132
Sanghoon Lee <gcccompil3r@gmail.com>
2023-08-11 11:32:02 +0900

    chore: [이상훈] 필요 없는 @PropertySource 제거 [IDY-4]

--- src/main/java/com/dyes/backend/config/CorsConfig.java
+++ src/main/java/com/dyes/backend/config/CorsConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-@PropertySource("classpath:cors.properties")
 public class CorsConfig implements WebMvcConfigurer {
 
     private final PropertyUtil propertyUtil;
