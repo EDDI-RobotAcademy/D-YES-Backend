694204157fbb115022b93a04ad3486eb5ba3fe30
Sanghoon Lee <gcccompil3r@gmail.com>
2023-08-11 09:59:17 +0900

    Update MySqlConfig.java

--- src/main/java/com/dyes/backend/config/MySqlConfig.java
+++ src/main/java/com/dyes/backend/config/MySqlConfig.java
@@ -13,7 +13,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@PropertySource(value = "mysql.properties")
+@PropertySource(value = "classpath:mysql.properties")
 @Configuration
 public class MySqlConfig {
 
