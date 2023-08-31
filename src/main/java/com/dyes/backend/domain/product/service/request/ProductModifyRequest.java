64d69cfe2adaf9c09db327725f594d5e134bf001
jeongdawun <jeongdawun33@gmail.com>
2023-09-01 08:21:01 +0900

    update: [정다운] 관리자가 상품 수정시 상품, 옵션 판매 상태를 업데이트하는 기능 [SDY-24]

--- src/main/java/com/dyes/backend/domain/product/service/request/ProductModifyRequest.java
+++ src/main/java/com/dyes/backend/domain/product/service/request/ProductModifyRequest.java
@@ -1,6 +1,7 @@
 package com.dyes.backend.domain.product.service.request;
 
 import com.dyes.backend.domain.product.entity.CultivationMethod;
+import com.dyes.backend.domain.product.entity.SaleStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,5 @@ public class ProductModifyRequest {
     private String productName;
     private String productDescription;
     private CultivationMethod cultivationMethod;
+    private SaleStatus productSaleStatus;
 }
