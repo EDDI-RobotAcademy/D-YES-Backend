c26109d24910b5bd16833e35aee61c9eac32b6a8
jeongdawun <jeongdawun33@gmail.com>
2023-09-04 22:51:39 +0900

    chore: [정다운] 장바구니에 담은 상품도 삭제할 수 있도록 변경, 누락된 파일 추가 [SDY-63]

--- src/main/java/com/dyes/backend/domain/cart/repository/ContainProductOptionRepository.java
+++ src/main/java/com/dyes/backend/domain/cart/repository/ContainProductOptionRepository.java
@@ -2,6 +2,7 @@ package com.dyes.backend.domain.cart.repository;
 
 import com.dyes.backend.domain.cart.entity.Cart;
 import com.dyes.backend.domain.cart.entity.ContainProductOption;
+import com.dyes.backend.domain.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +16,7 @@ public interface ContainProductOptionRepository extends JpaRepository<ContainPro
 
     @Query("select cpo FROM ContainProductOption cpo join fetch cpo.cart cart join fetch cpo.productOption po join fetch po.product where cart = :cart")
     List<ContainProductOption> findAllByCartWithProduct(@Param("cart") Cart cart);
+
+    @Query("select cpo FROM ContainProductOption cpo join fetch cpo.productOption where cpo.productOption = :productOption")
+    List<ContainProductOption> findAllByProductOption(ProductOption productOption);
 }
