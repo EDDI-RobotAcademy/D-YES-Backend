81c10959f977d979e8e0b2a95731d336574fdde0
jeongdawun <jeongdawun33@gmail.com>
2023-09-11 10:08:56 +0900

    update: [정다운] 지역별 상품 찾기 쿼리문 변경 [SDY-81]

--- src/main/java/com/dyes/backend/domain/farm/repository/FarmCustomerServiceInfoRepository.java
+++ src/main/java/com/dyes/backend/domain/farm/repository/FarmCustomerServiceInfoRepository.java
@@ -4,12 +4,13 @@ import com.dyes.backend.domain.farm.entity.Farm;
 import com.dyes.backend.domain.farm.entity.FarmCustomerServiceInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FarmCustomerServiceInfoRepository extends JpaRepository<FarmCustomerServiceInfo, Long> {
     FarmCustomerServiceInfo findByFarm(Farm farm);
 
-    @Query("select fcs FROM FarmCustomerServiceInfo fcs join fetch fcs.farm")
-    List<FarmCustomerServiceInfo> findByFarmAddressAddressContaining(String farmLocation);
+    @Query("select fcs FROM FarmCustomerServiceInfo fcs join fetch fcs.farm where fcs.farmAddress.address like %:region%")
+    List<FarmCustomerServiceInfo> findByFarmAddressAddressContaining(@Param("region") String region);
 }
