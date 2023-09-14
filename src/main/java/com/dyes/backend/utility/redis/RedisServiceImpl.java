fd32581c0f7e01690c4166bcd2dd5903a95c15e8
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-09-14 14:04:58 +0900

    fix: [유경우] redis deletePaymentTemporarySaveData 메서드 누락 추가[SDY-91]

--- src/main/java/com/dyes/backend/utility/redis/RedisServiceImpl.java
+++ src/main/java/com/dyes/backend/utility/redis/RedisServiceImpl.java
@@ -1,17 +1,14 @@
 package com.dyes.backend.utility.redis;
 
-import com.dyes.backend.domain.order.service.user.request.PaymentTemporarySaveRequest;
+import com.dyes.backend.domain.payment.service.request.PaymentTemporarySaveRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.lettuce.core.RedisException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
 
 @Slf4j
 @Service
@@ -48,21 +45,44 @@ public class RedisServiceImpl implements RedisService{
 
     @Override
     public void paymentTemporarySaveData(String id, PaymentTemporarySaveRequest saveRequest) throws JsonProcessingException {
+        log.info("paymentTemporarySaveData start");
         ValueOperations<String, String> value = redisTemplate.opsForValue();
         String saveData = objectMapper.writeValueAsString(saveRequest);
         value.set(id, saveData);
+        log.info("paymentTemporarySaveData end");
+
     }
 
     public PaymentTemporarySaveRequest getPaymentTemporarySaveData (String id) throws JsonProcessingException {
+        log.info("getPaymentTemporarySaveData start");
+
         ValueOperations<String, String> value = redisTemplate.opsForValue();
 
         String savedData = value.get(id);
 
         if (savedData == null) {
+            log.info("getPaymentTemporarySaveData end");
+
             return null;
         }
         PaymentTemporarySaveRequest saveData = objectMapper.readValue(savedData, PaymentTemporarySaveRequest.class);
+        log.info("getPaymentTemporarySaveData end");
 
         return saveData;
+    }
+
+    @Override
+    public boolean deletePaymentTemporarySaveData(String id) {
+        log.info("deletePaymentTemporarySaveData start");
+
+        try {
+            redisTemplate.delete(id);
+            log.info("deletePaymentTemporarySaveData end");
+
+            return true;
+        } catch (Exception e) {
+            log.error("Error while deleting payment temporary save data: {}", e.getMessage(), e);
+            return false;
+        }
     }
 }
