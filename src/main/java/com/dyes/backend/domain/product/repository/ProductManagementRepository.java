80baafb8304634c3d9ed6928a856309fff67c1bf
jeongdawun <jeongdawun33@gmail.com>
2023-09-20 11:51:27 +0900

    update: [정다운] 사용자의 신규 상품 10개 목록 조회 기능에서 7일로 조회 조건을 변경 [SDY-99]

--- src/main/java/com/dyes/backend/domain/product/repository/ProductManagementRepository.java
+++ src/main/java/com/dyes/backend/domain/product/repository/ProductManagementRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProductManagementRepository extends JpaRepository<ProductManagement, Long> {
-    @Query("select pm FROM ProductManagement pm join fetch pm.product p join fetch p.farm")
-    Page<ProductManagement> findNew10ByCreatedDate(Pageable pageable);
+    @Query("select pm FROM ProductManagement pm join fetch pm.product p join fetch p.farm where pm.createdDate between :startDate and :endDate")
+    Page<ProductManagement> findByCreatedDateBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
 
     List<ProductManagement> findAllByOrderByCreatedDateAsc();
 
