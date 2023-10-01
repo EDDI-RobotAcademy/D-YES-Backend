ab3cfe4c0c0f625ed442862f6cf7abe29fa5e476
jeongdawun <jeongdawun33@gmail.com>
2023-10-01 21:19:22 +0900

    fix: [정다운] 레시피에 등록된 댓글 목록 조회 기능에 역직렬화 문제 해결 [SDY-152]

--- src/main/java/com/dyes/backend/domain/recipe/service/response/RecipeCommentInfoResponse.java
+++ src/main/java/com/dyes/backend/domain/recipe/service/response/RecipeCommentInfoResponse.java
@@ -1,5 +1,6 @@
 package com.dyes.backend.domain.recipe.service.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 public class RecipeCommentInfoResponse {
     private Long commentId;
     private String nickName;
+    @JsonProperty("isMyRecipeComment")
     private boolean isMyRecipeComment;
     private String content;
     private Boolean isDeleted;
